a601f59d0e051effaf2c9c43e5103f18b4139cda
maryiasahanovich <m.s.saganovich@gmail.com>
2024-08-06 22:58:53 +0200

    confirmTest, hideTest, showTest are added

--- src/test/java/com/itacademy/NewWebsiteTest.java
+++ src/test/java/com/itacademy/NewWebsiteTest.java
@@ -3,6 +3,8 @@ package com.itacademy;
 import com.itacademy.utils.ActionsUtils;
 import com.itacademy.utils.ScreenshotUtils;
 import com.itacademy.utils.Waiters;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.*;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -11,15 +13,17 @@ import java.util.List;
 
 public class NewWebsiteTest extends BaseTest{
 
+    private static final Logger LOGGER = LogManager.getLogger(NewWebsiteTest.class);
+
     @Test
     public void  frametest() {
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
         Waiters.wait(3000);
         List<WebElement> list = driver.findElements(By.xpath("//*[text() = 'All Access plan']"));
-        System.out.println(list.size());
+        LOGGER.info(list.size());
         ScreenshotUtils.makeScreenshot(driver, "frame_");
         driver.switchTo().frame("courses-iframe");
-        System.out.println(list.size());
+        LOGGER.info(list.size());
         driver.switchTo().defaultContent();
 
     }
@@ -27,12 +31,50 @@ public class NewWebsiteTest extends BaseTest{
     @Test
     public void alertTest() {
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.findElement(By.id("name")).sendKeys("Maria");
         driver.findElement(By.id("alertbtn")).click();
         Alert alert = driver.switchTo().alert();
-        System.out.println(alert.getText());
-        alert.accept(); //[ok]
-        //alert.dismiss(); //[cancel]
+        LOGGER.info(alert.getText());
+        alert.accept();
 
+    }
+
+    @Test
+    public void confirmTest() {
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        WebElement nameInput = driver.findElement(By.id("name"));
+        LOGGER.info(nameInput.getAttribute("placeholder"));
+        nameInput.sendKeys("Maria");
+        driver.findElement(By.id("confirmbtn")).click();
+        Alert alert = driver.switchTo().alert();
+        LOGGER.info(alert.getText());
+        alert.accept();
+        driver.findElement(By.id("confirmbtn")).click();
+        Alert alert1 = driver.switchTo().alert();
+        LOGGER.info(alert1.getText());
+        alert1.dismiss();
+
+    }
+
+    @Test
+    public void hideTest() {
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        WebElement showHide = driver.findElement(By.id("displayed-text"));
+        LOGGER.info(showHide.getAttribute("placeholder"));
+        Assert.assertTrue(showHide.isDisplayed());
+        driver.findElement(By.id("hide-textbox")).click();
+        Assert.assertFalse(showHide.isDisplayed());
+    }
+
+    @Test
+    public void showTest() {
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        WebElement showHide = driver.findElement(By.id("displayed-text"));
+        LOGGER.info(showHide.getAttribute("placeholder"));
+        driver.findElement(By.id("hide-textbox")).click();
+        Assert.assertFalse(showHide.isDisplayed());
+        driver.findElement(By.id("show-textbox")).click();
+        Assert.assertTrue(showHide.isDisplayed());
     }
 
     @Test
