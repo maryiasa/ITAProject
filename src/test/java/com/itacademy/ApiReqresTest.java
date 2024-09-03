ec42c151d357d7361fb7a549c10d41c3c1d77e38
maryiasahanovich <m.s.saganovich@gmail.com>
2024-09-03 18:00:08 +0200

    ApiReqresTest is updated to log Request And Response If Validation Fails. Log request uri and status line only for passed tests

--- src/test/java/com/itacademy/ApiReqresTest.java
+++ src/test/java/com/itacademy/ApiReqresTest.java
@@ -2,6 +2,8 @@ package com.itacademy;
 
 import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
+import io.restassured.config.LogConfig;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
@@ -30,63 +32,231 @@ public class ApiReqresTest {
         RestAssured.baseURI = "https://reqres.in";
     }
 
+    /*@Test
+    public void getUsers() {
+        log.info("getUsers BEGIN");
+        Response response = given().log().uri()
+                .queryParam("page", "2")
+                .when()
+                .get("api/users")
+                .then().log().all()
+                .assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("json/getUsersJsonSchema.json"))
+                .extract().response();
+        assertEquals(response.getStatusCode(), 200);
+        if (response.getStatusCode() == 200) {
+            log.info("getUsers END: " + response.getStatusLine());
+        } else log.error("getUsers ERROR:\n " + response.getStatusLine() + "\n" + response.getHeaders() + "\n" + response.asString());
 
+    }
+*/
     @Test
     public void getUsers() {
-        given().log().uri()
+        log.info("getUsers BEGIN");
+        given().config(RestAssured.config().logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL)))
+                .log().uri()
                 .queryParam("page", "2")
                 .when().get("api/users")
-                .then().log().all().statusCode(200)
+                .then().log().status()
+                .statusCode(201)
                 .assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("json/getUsersJsonSchema.json"));
+        log.info("getUsers END");
     }
-
 
     @Test
     public void getSingleUser() {
-        given().log().uri()
+        log.info("getSingleUser BEGIN");
+        given().config(RestAssured.config().logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL)))
+                .log().uri()
                 .queryParam("id", "9")
                 .when().get("api/users")
-                .then().log().all().statusCode(200)
+                .then().log().status()
+                .statusCode(200)
                 .body("data.id", equalTo(9))
                 .body("data.email", equalTo("tobias.funke@reqres.in"))
                 .assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("json/getSingleUserJsonSchema.json"));
+        log.info("getSingleUser END");
     }
 
     @Test
     public void getSingleUserNotFound() {
-        given().log().uri()
+        log.info("getSingleUserNotFound BEGIN");
+        given().config(RestAssured.config().logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL)))
+                .log().uri()
                 .pathParam("path", 23)
                 .when().get("api/users/{path}")
-                .then().log().all().statusCode(404)
+                .then().log().status()
+                .statusCode(404)
                 .body("isEmpty()", Matchers.is(true));
+        log.info("getSingleUserNotFound END");
+    }
+
+    @Test
+    public void getListOfResources() {
+        log.info("getListOfResources BEGIN");
+        given().config(RestAssured.config().logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL)))
+                .log().uri()
+                .when().get("api/unknown")
+                .then().log().status().statusCode(200)
+                .assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("json/getListOfResourcesJsonSchema.json"));
+        log.info("getListOfResources END");
+    }
+
+    @Test
+    public void getSingleResource() {
+        log.info("getSingleResource BEGIN");
+        given().config(RestAssured.config().logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL)))
+                .log().uri()
+                .pathParam("path", 2)
+                .when().get("api/unknown/{path}")
+                .then().log().status().statusCode(200)
+                .assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("json/getSingleResourceJsonSchema.json"));
+        log.info("getSingleResource END");
+
+    }
+
+    @Test
+    public void getSingleResourceIsNotFound() {
+        log.info("getSingleResourceIsNotFound BEGIN");
+        given().config(RestAssured.config().logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL)))
+                .log().uri()
+                .pathParam("path", 23)
+                .when().get("api/unknown/{path}")
+                .then().log().status().statusCode(404)
+                .body("isEmpty()", Matchers.is(true));
+        log.info("getSingleResourceIsNotFound END");
     }
 
     @Test
     public void postCreateUser() {
+        log.info("postCreateUser BEGIN");
         File file = new File("src/test/resources/json/user.json");
-        given().log().uri().contentType(ContentType.JSON).body(file)
+        given().config(RestAssured.config().logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL)))
+                .log().uri()
+                .contentType(ContentType.JSON).body(file)
                 .when().post("api/users")
-                .then().log().all().statusCode(201)
+                .then().log().status().statusCode(201)
                 .assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("json/postCreateUserJsonSchema.json"));
+        log.info("postCreateUser END");
     }
 
     @Test
     public void putUpdateUser() {
+        log.info("putUpdateUser BEGIN");
         Faker faker = new Faker();
         Map<String, String> userBody = new HashMap<>();
         userBody.put("name", faker.name().firstName());
         userBody.put("job", faker.job().title());
-        given().log().uri().contentType(ContentType.JSON).body(userBody)
+        given().config(RestAssured.config().logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL)))
+                .log().uri()
+                .contentType(ContentType.JSON).body(userBody)
                 .pathParam("path", "2")
                 .when().put("api/users/{path}")
-                .then().log().all().statusCode(200)
+                .then().log().status().statusCode(200)
                 .assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("json/putUpdateUserJsonSchema.json"));
+        log.info("putUpdateUser END");
 
     }
 
     @Test
+    public void patchUpdateUser() {
+        log.info("patchUpdateUser BEGIN");
+        Faker faker = new Faker();
+        Map<String, String> userBody = new HashMap<>();
+        userBody.put("name", faker.name().firstName());
+        given().config(RestAssured.config().logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL)))
+                .log().uri()
+                .contentType(ContentType.JSON).body(userBody)
+                .pathParam("path", "2")
+                .when().patch("api/users/{path}")
+                .then().log().status().statusCode(200);
+        log.info("patchUpdateUser END");
+    }
+
+    @Test
+    public void deleteUser() {
+        log.info("deleteUser BEGIN");
+        given().config(RestAssured.config().logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL)))
+                .log().uri()
+                .pathParam("path", 2)
+                .when().delete("api/unknown/{path}")
+                .then().log().status().statusCode(204)
+                .header("Content-Length", "0");
+        log.info("deleteUser END");
+    }
+
+    @Test
+    public void postRegisterSuccessful() {
+        log.info("postRegisterSuccessful BEGIN");
+        File file = new File("src/test/resources/json/registerUser.json");
+        given().config(RestAssured.config().logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL)))
+                .log().uri()
+                .contentType(ContentType.JSON)
+                .body(file)
+                .when().post("api/register")
+                .then().log().status().statusCode(200)
+                .assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("json/postRegisterSuccessfulJsonSchema.json"));
+        log.info("postRegisterSuccessful END");
+    }
+
+    @Test
+    public void postRegisterUnsuccessful() {
+        log.info("postRegisterUnsuccessful BEGIN");
+        File file = new File("src/test/resources/json/registerUserUnsuccessful.json");
+        given().config(RestAssured.config().logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL)))
+                .log().uri()
+                .contentType(ContentType.JSON)
+                .body(file)
+                .when().post("api/register")
+                .then().log().status().statusCode(400)
+                .body("error",  equalTo("Missing password"));
+        log.info("postRegisterUnsuccessful END");
+    }
+
+    @Test
+    public void postLoginSuccessful() {
+        log.info("postLoginSuccessful BEGIN");
+        File file = new File("src/test/resources/json/registerUser.json");
+        given().config(RestAssured.config().logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL)))
+                .log().uri()
+                .contentType(ContentType.JSON)
+                .body(file)
+                .when().post("api/login")
+                .then().log().status().statusCode(200)
+                .assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("json/postLoginSuccessfulJsonSchema.json"));
+        log.info("postLoginSuccessful END");
+    }
+
+    @Test
+    public void postLoginUnsuccessful() {
+        log.info("postLoginUnsuccessful BEGIN");
+        File file = new File("src/test/resources/json/registerUserUnsuccessful.json");
+        given().config(RestAssured.config().logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL)))
+                .log().uri()
+                .contentType(ContentType.JSON)
+                .body(file)
+                .when().post("api/login")
+                .then().log().status().statusCode(400)
+                .body("error",  equalTo("Missing password"));
+        log.info("postLoginUnsuccessful END");
+    }
+
+    @Test
+    public void getDelayResponse() {
+        log.info("getDelayResponse BEGIN");
+        given().config(RestAssured.config().logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL)))
+                .log().uri()
+                .queryParam("delay", "3")
+                .when().get("api/users")
+                .then().log().status().statusCode(200)
+                .assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("json/getUsersJsonSchema.json"));
+        log.info("getDelayResponse END");
+    }
+
+    @Test
     public void combineGetAndPutUser() {
-        Response response = given().log().uri().queryParam("id", "9").when().get("api/users").then().log().all()
+        Response response = given().config(RestAssured.config().logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL)))
+                .log().uri()
+                .queryParam("id", "9").when().get("api/users").then().log().status()
                 .assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("json/getSingleUserJsonSchema.json"))
                 .extract().response();
 
@@ -103,7 +273,9 @@ public class ApiReqresTest {
         userBody.put("name", faker.name().firstName());
         userBody.put("job", faker.job().title());
         String responseName =
-        given().log().uri().contentType(ContentType.JSON).body(userBody)
+                given().config(RestAssured.config().logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL)))
+                .log().uri()
+                .contentType(ContentType.JSON).body(userBody)
                 .pathParam("path", "2")
                 .queryParam("id", "9")
                 .when().put("api/users/{path}").jsonPath().getString("name");
